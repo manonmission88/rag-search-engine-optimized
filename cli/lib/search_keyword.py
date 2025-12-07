4fc62bebbf51051c64be2ce8d93b6bd94383f07e
manonmission88 <manishnewray88@gmail.com>
2025-12-06 21:32:59 -0800

    added stemming:

--- cli/lib/search_keyword.py
+++ cli/lib/search_keyword.py
@@ -1,10 +1,11 @@
-from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
+from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies,load_stop_words
 import string
+from nltk.stem import PorterStemmer
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
-    updated_query = preprocess_text(query)
+    updated_query = filter_query(preprocess_text(query).split(" "))
     for movie in movies:
         updated_movie = preprocess_text(movie['title'])
         if tokenization_compare(updated_query,updated_movie):
@@ -27,4 +28,12 @@ def tokenization_compare(text1: str,text2:  str)->list:
     return False 
     
     
-    
+def filter_query(query):
+    stop_words = load_stop_words()
+    stemmer = PorterStemmer()
+    new_query = []
+    for word in query:
+        if word in stop_words:
+            continue 
+        new_query.append(word)
+    return " ".join([stemmer.stem(q) for q in new_query]) 
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -5,9 +5,14 @@ DEFAULT_SEARCH_LIMIT = 5
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
-
+DATA_PATH1 = os.path.join(PROJECT_ROOT, "data", "stop_words.txt")
 
 def load_movies() -> list[dict]:
     with open(DATA_PATH, "r") as f:
         data = json.load(f)
     return data["movies"]
+
+def load_stop_words() -> list:
+    with open(DATA_PATH1, "r") as f:
+        data = f.read().splitlines()
+    return data
