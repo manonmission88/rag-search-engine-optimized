ddf38a1dc1b620656d9f860d5f56f45c576f9059
manonmission88 <manishnewray88@gmail.com>
2025-12-06 21:05:18 -0800

    added tokenization

--- cli/lib/search_keyword.py
+++ cli/lib/search_keyword.py
@@ -4,10 +4,10 @@ import string
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
+    updated_query = preprocess_text(query)
     for movie in movies:
-        updated_query = preprocess_text(query)
         updated_movie = preprocess_text(movie['title'])
-        if updated_query in updated_movie:
+        if tokenization_compare(updated_query,updated_movie):
             results.append(movie)
             if len(results) >= limit:
                 break
@@ -16,5 +16,15 @@ def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
 def preprocess_text(text: str)-> str:
     text = text.lower().translate(str.maketrans("", "", string.punctuation))
     return text
+    
+def tokenization_compare(text1: str,text2:  str)->list:
+    updated_text1 = text1.split(" ")
+    updated_text2 = text2.split(" ")
+    for query_text in updated_text1:
+        for title_text in updated_text2:
+            if query_text in title_text:
+                return True 
+    return False 
+    
     
     
