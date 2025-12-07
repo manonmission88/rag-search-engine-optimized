a693d2a3f9a4adce09d8e4fe1851aa3ac910acce
manonmission88 <manishnewray88@gmail.com>
2025-12-06 20:47:05 -0800

    preprocess tex

--- cli/lib/search_keyword.py
+++ cli/lib/search_keyword.py
@@ -1,12 +1,20 @@
 from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
-
+import string
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
     for movie in movies:
-        if query in movie["title"]:
+        updated_query = preprocess_text(query)
+        updated_movie = preprocess_text(movie['title'])
+        if updated_query in updated_movie:
             results.append(movie)
             if len(results) >= limit:
                 break
     return results
+
+def preprocess_text(text: str)-> str:
+    text = text.lower().translate(str.maketrans("", "", string.punctuation))
+    return text
+    
+    
