589f19671c703a1b260e718b16b5b6662dcad42b
manonmission88 <manishnewray88@gmail.com>
2025-12-17 18:18:40 -0800

    implemented llm

---
+++ cli/test_gemini.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+model = 'gemini-2.5-flash'
+prompt = "Why is Boot.dev such a great place to learn about RAG? Use one max"
+
+if __name__ == "__main__":
+    response = client.models.generate_content(
+        model= model, 
+        contents= prompt
+    )
+    assert response.usage_metadata is not None 
+    print(response.text)
+    print(f"Prompt Tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response Tokens: {response.usage_metadata.candidates_token_count}")
